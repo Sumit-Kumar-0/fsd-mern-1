8b3d46a778130749a8cca80e20080cbfbce8c5cf
Sumit Kumar <sumit.handysolver@gmail.com>
2023-10-24 23:38:15 +0530

    working on delete page

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -4,7 +4,11 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const MONGO_URL = process.env.MONGO_URL
+const MONGO_DB = process.env.MONGO_DB;
+const MONGO_DB_NAME = process.env.MONGO_DB_NAME;
+const MONGO_USER = process.env.MONGO_USER;
+const MONGO_PASSWORD = process.env.MONGO_PASSWORD;
+const MONGO_URL = `mongodb+srv://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_DB}.phqufut.mongodb.net/${MONGO_DB_NAME}`
 
 const connectDB = async () => {
     try{
